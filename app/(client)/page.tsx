5fbc4dba4e639e6128ca1dcf5e72a528b6b51388
KramYengir <kramyengir@gmail.com>
2024-05-08 13:48:39 +0100

    start implementing data from queries

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,5 +1,19 @@
-const Home = () => {
-  return <div>Homepage</div>;
+import Review from "../interfaces/ReviewType";
+import * as fetcher from "../utils/Qeuries";
+
+const Home = async () => {
+  const reviews: Review[] = await fetcher.getAllReviews();
+  console.log(reviews);
+
+  return (
+    <div>
+      {reviews?.length > 0 &&
+        reviews.map((review, index) => (
+          // Replace this with your actual JSX for each review
+          <h2 key={index}>{review.title}</h2>
+        ))}
+    </div>
+  );
 };
 
 export default Home;
