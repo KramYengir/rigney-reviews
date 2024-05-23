b6a4c9fe0d00cf96e1bbe5ffbedeec202f9a7a4f
KramYengir <kramyengir@gmail.com>
2024-05-23 13:59:54 +0100

    shadows on filter bar & padding above footer

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       >
         <Provider>
           <Navbar />
-          <main className=" max-w-5xl mx-auto px-6">{children}</main>
+          <main className=" max-w-5xl mx-auto px-6 pb-8">{children}</main>
           <Footer />
         </Provider>
       </body>
--- app/components/FilterBar.tsx
+++ app/components/FilterBar.tsx
@@ -12,22 +12,22 @@ const FilterBar = () => {
   }, []);
 
   return (
-    <div className="flex  justify-end gap-2">
+    <div className="flex  justify-end gap-2 text-center">
       <Link
         href={"/"}
-        className={`rounded-b-md px-2 py-1 ${currentPath === "/" ? " bg-scooter-600 text-scooter-50 dark:bg-scooter-50 dark:text-scooter-900" : ""}`}
+        className={`rounded-b-md px-2 py-1 min-w-12 shadow-sm shadow-scooter-950 dark:shadow-scooter-50 ${currentPath === "/" ? " bg-scooter-600 text-scooter-50 dark:bg-scooter-50 dark:text-scooter-900" : ""}`}
       >
         All
       </Link>
       <Link
         href={"/film"}
-        className={`rounded-b-md px-2 py-1 ${currentPath === "/film" ? " bg-scooter-600 text-scooter-50 dark:bg-scooter-50 dark:text-scooter-900" : ""}`}
+        className={`rounded-b-md px-2 py-1 min-w-12 shadow-sm shadow-scooter-950 dark:shadow-scooter-50 ${currentPath === "/film" ? " bg-scooter-600 text-scooter-50 dark:bg-scooter-50 dark:text-scooter-900" : ""}`}
       >
         Film
       </Link>
       <Link
         href={"/tv"}
-        className={`rounded-b-md px-2 py-1 ${currentPath === "/tv" ? " bg-scooter-600 text-scooter-50 dark:bg-scooter-50 dark:text-scooter-900" : ""}`}
+        className={`rounded-b-md px-2 py-1 min-w-12 shadow-sm shadow-scooter-950 dark:shadow-scooter-50 ${currentPath === "/tv" ? " bg-scooter-600 text-scooter-50 dark:bg-scooter-50 dark:text-scooter-900" : ""}`}
       >
         TV
       </Link>
