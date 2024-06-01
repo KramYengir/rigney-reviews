fcbba643ee23c2ffd866d1f9b7376ccba79dc4f8
KramYengir <kramyengir@gmail.com>
2024-06-01 18:22:10 +0100

    update revalidate time

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   description: "Rigney Reviews - The best reviews on the internet",
 };
 
-export const revalidate = 36; //re fetch data every hour
+export const revalidate = 3600; //re fetch data every hour
 
 const Home = async () => {
   const reviews: Review[] = await getAllReviews();
