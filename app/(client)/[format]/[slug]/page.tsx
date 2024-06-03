a778aceeede6a42d061f8c2a6a65803aa566373c
KramYengir <kramyengir@gmail.com>
2024-06-03 14:29:39 +0100

    add breadcrumb style back links

--- app/(client)/[format]/[slug]/page.tsx
+++ app/(client)/[format]/[slug]/page.tsx
@@ -10,6 +10,7 @@ import { notFound } from "next/navigation";
 import type { Metadata } from "next";
 import QuickSynopsis from "@/app/components/QuickSynopsis";
 import Verdict from "@/app/components/Verdict";
+import DynamicBackLink from "@/app/components/DynamicBackLink";
 
 export const metadata: Metadata = {
   title: "RReviews",
@@ -38,11 +39,7 @@ const page = async ({ params }: Props) => {
 
   return (
     <article className="grid pt-2 pb-10">
-      <div className="flex justify-start">
-        <Link href={`/`} className="btn-primary">
-          Back
-        </Link>
-      </div>
+      <DynamicBackLink review={review} />
       <h2 className="mt-12 text-2xl md:text-4xl font-semibold text-scooter-800 dark:text-scooter-50">
         {review?.title}
       </h2>
@@ -65,11 +62,7 @@ const page = async ({ params }: Props) => {
       {/* tetsing api integration */}
       <ProductionInfo title={review.title} format={type} />
       <hr className="my-4 border-b border-slate-300" />
-      <div className="flex justify-start">
-        <Link href={`/`} className="btn-primary">
-          Back
-        </Link>
-      </div>
+      <DynamicBackLink review={review} />
     </article>
   );
 };
---
+++ app/components/DynamicBackLink.tsx
@@ -0,0 +1,33 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import Review from "../interfaces/ReviewType";
+import { usePathname } from "next/navigation";
+
+interface Props {
+  review: Review;
+}
+
+const DynamicBackLink = ({ review }: Props) => {
+  const path = usePathname();
+  const link = path.includes("masterpieces")
+    ? "masterpieces"
+    : path.includes("hidden-gems")
+      ? "hidden-gems"
+      : review.format;
+
+  return (
+    <div className="flex justify-start items-center gap-2 capitalize font-extralight ">
+      &gt;
+      <Link href={`/`} className="hover:font-medium">
+        Home
+      </Link>
+      /
+      <Link href={`/${review.format}`} className="hover:font-medium">
+        {link}
+      </Link>
+    </div>
+  );
+};
+
+export default DynamicBackLink;
