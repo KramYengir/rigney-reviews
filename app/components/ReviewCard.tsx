78279e290597de2bc27f7703e54685e1316bf954
KramYengir <kramyengir@gmail.com>
2024-06-03 14:38:05 +0100

    add shadows to cards

--- app/components/ReviewCard.tsx
+++ app/components/ReviewCard.tsx
@@ -13,7 +13,7 @@ const ReviewCard = ({ review }: Props) => {
 
   return (
     <DynamicLink review={review}>
-      <article className="card-gradient relative flex flex-col min-h-40 min-w-72 sm:max-w-[310px] p-4 rounded-xl text-slate-50 hover:scale-[1.005] ">
+      <article className="card-gradient relative flex flex-col min-h-40 min-w-72 sm:max-w-[310px] p-4 rounded-xl text-slate-50 shadow-md shadow-scooter-950 dark:shadow-scooter-900 hover:scale-[1.005] ">
         <header className="flex justify-between items-center text-lg font-semibold">
           <h3>{review?.title}</h3>
           <p>{review.rating}</p>
